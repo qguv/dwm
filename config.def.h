d8a76b00b96b30e99bfb48e8142780413bf74c3c
Quint Guvernator <quint@guvernator.net>
2018-12-11 13:02:40 -0500

    use 50% translucent top bar

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const unsigned int baralpha = 0xd0;
+static const unsigned int baralpha = 0x80;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
